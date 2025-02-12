71be718bdc3125b5f38bb6163b972ea2b8d21fba
FathanJundiR <fathanjundi43@gmail.com>
2025-02-12 22:11:26 +0700

    commit package json

--- Frontend/elearning-unipi/vite.config.ts
+++ Frontend/elearning-unipi/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = { target: '18'};
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        ["babel-plugin-react-compiler", ReactCompilerConfig],
+      ],
+    },
+  })],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 })
